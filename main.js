a5502e7a5a742977dbdb47e7e81010b31a90d76b
Lanzoor <166265896+Lanzoor@users.noreply.github.com>
2025-04-22 19:17:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let anchorLinks = document.querySelectorAll('a[href^="#"]');
 let scrollButtons = document.querySelectorAll('button[data-target]');
 let backToTop = document.getElementById('back-to-top');
 let modal = document.querySelector('.modal');
+let pronounsCheck = document.getElementById('check_pronouns');
+let singularPronouns = document.getElementById('sing_pronouns');
+let pluralPronouns = document.getElementById('plur_pronouns');
+let pronounsStatus = document.getElementById('stat_pronouns');
 
 anchorLinks.forEach(link => {
     let targetId = link.hash.slice(1);
@@ -44,3 +48,15 @@ if (modal) {
         modal.classList.add('hidden');
     }
 }
+
+pronounsCheck.addEventListener('change', () => {
+    if (pronounsCheck.checked) {
+        singularPronouns.classList.add("hidden");
+        pluralPronouns.classList.remove("hidden");
+        pronounsStatus.innerText = "plural pronouns";
+    } else {
+        singularPronouns.classList.remove("hidden");
+        pluralPronouns.classList.add("hidden");
+        pronounsStatus.innerText = "singular pronouns";
+    }
+})
